7c8216f236ea3a7c5e527a6c0e95950213600240
w <wires@phase.meta>
2019-06-15 20:13:14 +0200

    basic idea and some code

--- README.md
+++ README.md
@@ -1,15 +1,35 @@
-# download from github (including private)
+# Clone all repos from Github (including private)
 
-authenticate
+#### Installation
 
-	github-dl --login
+	npm install github-dl
 
-download all from organization
+#### Usage
 
-	github-dl -O statebox -d statebox/
+Clone all repos (including private) from user `wires` into folder `repos`:
 
-download all from user
+    github-dl wires -G -d repos
 
-	github-dl -u wires -d wires/
+Options:
 
+- `--destination`, `-d` set destination folder, will be created if doesn't exist
+- `--dotGit`, `-G` clone repositories as `[name].git` instead of just `name`
 
+When you run the command, it will ask for your github credentials (to get the list of repositories)
+
+```
+? Username wires
+? Password ************************
+? 2FA 123456
+```
+
+It now gets the list of repositories and stores them in two files in the target folder.
+
+1. `repo-data.json` all information and meta data for each repo
+2. `repo-urls.txt` just a list of git URLs for each repo
+
+To download all the repos (assuming we ran with `-d repos`), run
+
+	cat repos/repo-urls.txt | xargs -n1 -P8 git clone
+
+(this will run 8 processes in parallel, one git clone per line of the file)
--- github-dl.js
+++ github-dl.js
@@ -1,9 +1,26 @@
 let argv = require('minimist')(process.argv.slice(2))
+let R = require('ramda')
 let Octokit = require('@octokit/rest')
+console.log(argv)
 
-if (argv.login){
-    doLogin()
+let destination = argv.d || argv.destination || '.'
+let extension = (argv.dotGit || argv.G) ? '.git' : ''
+
+if(argv.help || (argv._.length <= 0)) {
+    console.log(`Usage (--help):
+
+Clone all repos (including private) from user 'wires' into folder 'repos':
+
+    github-dl wires -G -d repos
+
+Options:
+
+    --destination, -d       destination folder, will be created if doesn't exist
+    --dotGit, -G            store repos as 'name.git' instead of just 'name'
+`)
 }
+
+doDL(R.head(argv._), destination, extension)
 
 async function ask2FA() {
     return require('inquirer').prompt([
@@ -18,7 +35,12 @@ async function askUserPass() {
     ])
 }
 
-async function doLogin() {
+async function allRepos(user) {
+    let p = await octokit.repos.listForUser(user)
+    
+}
+
+async function doDL(user) {
     let answers = await askUserPass()
     
     let octokit = new Octokit({
@@ -34,7 +56,7 @@ async function doLogin() {
 
     try {
         let p = await octokit.activity.listPublicEvents()
-        console.log(p)
+        console.log(JSON.stringify(p))
     } catch (e) {
         console.error('Failed to login:', e.message)
         process.exit(-1)
